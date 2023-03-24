748e0573d9c0ce9afe8ff66db445e6e4d5911020
Seokyung <xuejd17@gmail.com>
2023-03-25 01:22:54 +0900

    Fix: Put deleteMessage Button & Method into Message component

--- src/components/messgaes/Message.js
+++ src/components/messgaes/Message.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { dbService } from "fbase";
+import { doc, deleteDoc } from "firebase/firestore";
 
-function Message({ msgObj }) {
+function Message({ msgObj, isOwner }) {
+	const deleteMessage = async (msgObj) => {
+		const isDelete = window.confirm(
+			`${msgObj.msgTitle} 메세지를 삭제하시겠습니까?`
+		);
+		if (isDelete) {
+			const msgRef = doc(
+				dbService,
+				"papers",
+				`${msgObj.paperId}`,
+				"messages",
+				`${msgObj.id}`
+			);
+			await deleteDoc(msgRef);
+			alert("메세지가 삭제되었습니다!");
+		}
+	};
+
 	return (
 		<div>
 			<h3>
@@ -9,6 +28,9 @@ function Message({ msgObj }) {
 			</h3>
 			<h4>{msgObj.msgWriter}</h4>
 			<p>{msgObj.msgContent}</p>
+			{isOwner && (
+				<button onClick={() => deleteMessage(msgObj)}>메세지 삭제</button>
+			)}
 		</div>
 	);
 }
--- src/components/messgaes/MessageList.js
+++ src/components/messgaes/MessageList.js
@@ -3,14 +3,11 @@ import { useParams } from "react-router-dom";
 import { authService, dbService } from "fbase";
 import { onAuthStateChanged } from "firebase/auth";
 import {
-	doc,
-	getDoc,
 	collection,
 	query,
 	where,
 	orderBy,
 	onSnapshot,
-	deleteDoc,
 } from "firebase/firestore";
 import Message from "components/messgaes/Message";
 
@@ -53,30 +50,11 @@ function MessageList({ userObj, paperCreator }) {
 		});
 	}, [paperCreator]);
 
-	const deleteMessage = async (message) => {
-		const isDelete = window.confirm(
-			`${message.msgTitle} 메세지를 삭제하시겠습니까?`
-		);
-		if (isDelete) {
-			const msgRef = doc(
-				dbService,
-				"papers",
-				`${message.paperId}`,
-				"messages",
-				`${message.id}`
-			);
-			await deleteDoc(msgRef);
-		}
-	};
-
 	return (
 		<div>
 			{messages.map((message) => (
 				<div key={message.id}>
-					<Message msgObj={message} />
-					{paperCreator === userObj.uid && (
-						<button onClick={() => deleteMessage(message)}>메세지 삭제</button>
-					)}
+					<Message msgObj={message} isOwner={paperCreator === userObj.uid} />
 				</div>
 			))}
 		</div>
