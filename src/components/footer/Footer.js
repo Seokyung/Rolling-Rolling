e43dc98fd28ed4d488fad1a30a6b2ba6cab74277
Seokyung <xuejd17@gmail.com>
2023-04-15 01:37:27 +0900

    Style: Add Navigation Bar and Menu Style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,21 @@
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
 	return (
 		<footer className="footer-container">
-			&copy; {new Date().getFullYear()} Rolling-Rolling
-			<br />
-			Seokyung Jee
+			<span>&copy; {new Date().getFullYear()} Rolling-Rolling</span>
+			<span className="footer-developer">
+				<a
+					href="https://github.com/Seokyung"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FontAwesomeIcon icon={faGithub} />
+				</a>
+				Seokyung Jee
+			</span>
 		</footer>
 	);
 }
--- src/components/menu/Navigation.js
+++ src/components/menu/Navigation.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { MenuOutlined } from "@ant-design/icons";
-import { Navbar, Offcanvas, Nav, NavDropdown } from "react-bootstrap";
-import logoImg from "assets/Rolling-Rolling_horizontal_logo.png";
+import { useSelector } from "react-redux";
+
+import logoImg from "assets/Rolling-Rolling_logo.png";
+import { Navbar, Offcanvas, Nav } from "react-bootstrap";
+import { List } from "react-bootstrap-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import "./Navigation.css";
 
 function Navigation() {
+	const userName = useSelector((state) => state.userReducer.displayName);
 	const [isMenu, setIsMenu] = useState(false);
 
 	const showMenu = () => {
 		setIsMenu((prev) => !prev);
+	};
+
+	const closeMenu = () => {
+		setIsMenu(false);
 	};
 
 	return (
@@ -18,27 +27,34 @@ function Navigation() {
 				<Link to={"/"} className="navigation-logo-container">
 					<img className="navigation-logo" src={logoImg} alt="logo" />
 				</Link>
-				<MenuOutlined className="navigation-menu" onClick={showMenu} />
+				<button className="navigation-menu" onClick={showMenu}>
+					<FontAwesomeIcon icon={faBars} />
+				</button>
 			</div>
 
 			<Navbar.Offcanvas placement="end" show={isMenu} onHide={showMenu}>
 				<Offcanvas.Header closeButton>
-					<Offcanvas.Title>Offcanvas</Offcanvas.Title>
+					<Offcanvas.Title>
+						<span className="navigation-offcanvas-username">{userName}</span>님
+					</Offcanvas.Title>
 				</Offcanvas.Header>
 				<Offcanvas.Body>
-					<Nav className="justify-content-end flex-grow-1 pe-3">
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/profile">프로필</Nav.Link>
-						<NavDropdown title="Dropdown">
-							<NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action4">
-								Another action
-							</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action5">
-								Something else here
-							</NavDropdown.Item>
-						</NavDropdown>
+					<Nav className="navigation-offcanvas-body">
+						<Link
+							to={"/"}
+							className="navigation-offcanvas-body-link"
+							onClick={closeMenu}
+						>
+							<FontAwesomeIcon icon={faHouse} />
+							페이퍼 홈
+						</Link>
+						<Link
+							to={"/profile"}
+							className="navigation-offcanvas-body-link"
+							onClick={closeMenu}
+						>
+							<FontAwesomeIcon icon={faUser} />내 정보
+						</Link>
 					</Nav>
 				</Offcanvas.Body>
 			</Navbar.Offcanvas>
